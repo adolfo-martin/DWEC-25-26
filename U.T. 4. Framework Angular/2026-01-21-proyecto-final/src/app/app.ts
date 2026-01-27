8a70af763535c7ecac50c61d77444d05c11e42ee
adolfo-martin <adolfo.martin@iesramonarcas.es>
2026-01-27 12:15:16 +0100

    hoy

--- U.T. 4. Framework Angular/2026-01-21-proyecto-final/src/app/app.ts
+++ U.T. 4. Framework Angular/2026-01-21-proyecto-final/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarApp } from "./components/navbar-app/navbar-app";
+import { PanelCategories } from "./components/panel-categories/panel-categories";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarApp],
+  imports: [RouterOutlet, NavbarApp, PanelCategories],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
